e5ab2f8aecc240bf5070cec73936ac7ef226a79e
Sinan Baltacioglu <siphon@gmail.com>
2019-01-30 00:47:31 -0500

    Major UI refactor. UI ready for back-end tie in.

--- static/micron/js/micron.js
+++ static/micron/js/micron.js
@@ -1,28 +1,223 @@
-$(function() {
+$(function() { 
+/*jq*/ 
+var micron = {
 
-    var recognition_lang = "en-US";
-    var recognition_lang_en = "en-US";
-    var recognition_lang_fr = "fr-CA";
+    debug_mode: false,
+    survey_index: 0,
+    question_index: 0,
+    recognition_lang: "en-US",
+    recognition_lang_en: "en-US",
+    recognition_lang_fr: "fr-CA",
+    lang_terms: {},
+    lang_terms_file: "",
+    lang_terms_en: "/static/data/can-live/micron-en.json",
+    lang_terms_fr: "/static/data/can-live/micron-fr.json",
+    surveys: [],
+    survey_file: "",
+    surveys_en: "/static/data/can-live/micron-surveys-en.json",
+    surveys_fr: "/static/data/can-live/micron-surveys-fr.json",
 
-    var lang_terms = {};
-    var lang_terms_en = "/static/data/can-live/micron-en.json";
-    var lang_terms_fr = "/static/data/can-live/micron-fr.json";
+    start_load_chain: function() {
+        if(micron.debug_mode == true) { console.log("fxn: start_load_chain"); }
+        micron.set_init_recog();
+        micron.getlang_json();
+    },
+    getlang_json: function() {
+        if(micron.debug_mode == true) { console.log("fxn: getlang_json"); }
+        var jsonload = micron.lang_terms_file;
+        $.getJSON(jsonload, function(json) {
+            //alert(JSON.stringify(json));
+            micron.lang_terms = json;
+            if(micron.debug_mode == true) { console.log("LANG FILE LOADED"); }
+            //console.log(json);
+            micron.getsurvey_json();
+        });
+    },
+    getsurvey_json: function() {
+        if(micron.debug_mode == true) { console.log("fxn: getsurvey_json"); }
+        var jsonload = micron.survey_file;
+        $.getJSON(jsonload, function(json) {
+            micron.surveys = json;
+            if(micron.debug_mode == true) { console.log("SURVEYS FILE LOADED"); }
+            //console.log(json);
+            micron.post_json_load();
+        });        
+    },
+    post_json_load: function() {
+        if(micron.debug_mode == true) { console.log("fxn: post_json_load"); }
+        micron.init_button_ctrl();
+        micron.languify();
+        micron.load_survey();
+        micron.ask_q();
+    },
+    languify_term: function(target, langterm) {
+        $("#" + target).text(langterm);
+        if(micron.debug_mode == true) { 
+            console.log("fxn: languify_term");
+            console.log("" + target + " " + langterm);
+        }
+    },
+    languify: function() {
 
-    var set_recog_en = function() {
-        recognition_lang = recognition_lang_en;
-        lang_terms = lang_terms_en;
-        $("#recognition_language").val(recognition_lang);
+        if(micron.debug_mode == true) { console.log("fxn: languify"); }
+        var lts = [
+            "lts_appname", "lts_appver", 
+            "lts_micronlisten", "lts_splashtext", "lts_footersplash", 
+            "lts_footermore", "lts_loading_msg", "lts_question_prompt", 
+            "lts_question_btn_prompt", "lts_answer_prompt", "lts_answer_btn_prompt",
+            "lts_thank_you"
+        ];
+        if(micron.debug_mode == true) { 
+            console.log("LANG");
+            console.log(micron.lang_terms); // this will show the info it in firebug console
+        }
+        for(var i = 0; i < lts.length; i++) {
+            micron.languify_term(lts[i], micron.lang_terms[lts[i]]);
+        }
+
+        var sels = ["survey", "survey_desc", "survey_q"];
+        //in survey_q:
+        var qels = ["qid", "qt", "qb", "a"];
+                
+        if(micron.debug_mode == true) { 
+            console.log("SURVEYS");
+            console.log(micron.surveys); // this will show the info it in firebug console
+        }
+        var survey = micron.surveys[micron.survey_index]; // only one for now, 
+        var sname = survey['survey_name'];
+        var sdesc = survey['survey_desc'];
+        var questions = survey['survey_q'];
+
+        $("#survey_title").text( sname );
+        $("#survey_desc").text( sdesc );
+        
+        //micron.load_survey();    
+    },
+    load_survey: function() {
+        if(micron.debug_mode == true) { console.log("fxn: load_survey"); }
+        //micron.ask_q();
+    },
+    answer_q: function() {
+        if(micron.debug_mode == true) { console.log("fxn: answer_q"); }
+        micron.qs_toggle(); 
+        $("#micron_answer").show();
+    },
+    ask_q: function() {
+        if(micron.debug_mode == true) { console.log("fxn: ask_q"); }
+        if(micron.surveys[micron.survey_index]['survey_q'].length > micron.question_index) {
+            micron.qs_toggle(); 
+            micron.load_question();
+            $("#micron_question").show();
+        } else {
+            if(micron.debug_mode == true) { console.log("MAX"); }
+            micron.thanks();
+        }
+    },
+    thanks: function() {
+        if(micron.debug_mode == true) { console.log("fxn: thanks"); }
+        micron.qs_toggle(); 
+        $("#micron_thanks").show();
+        $("#micron_answer").hide();
+    },
+    answer_and_get_next_q: function() { 
+        if(micron.debug_mode == true) { console.log("fxn: answer_and_get_next_q"); }
+
+        micron_answer_text = $("#micron_a").val();
+        micron_answer_qid = $("#micron_qid").val();
+
+        var submission_q = [{ 
+            'qid': micron_answer_qid
+            ,'a': micron_answer_text
+        }];
+        //var str_q = JSON.stringify(submission_q, null, 2);
+        var str_q = submission_q;
+        //
+        $.post( "/feedback", str_q)
+        .done(function( data ) {
+            if(micron.debug_mode == true) { console.log("imma submit! " + JSON.stringify(str_q)); }
+            // do end result from POST
+        });
+
+        micron.question_index++;
+        
+        micron.ask_q();
+    },
+    load_question: function() {
+        
+        if(micron.debug_mode == true) { 
+            console.log("fxn: load_question [s: " + micron.survey_index + " q: " + micron.question_index +"]");
+        }
+        var survey = micron.surveys[micron.survey_index]; // only one for now, 
+
+        var sname = survey['survey_name'];
+        var sdesc = survey['survey_desc'];
+        var questions = survey['survey_q'];
+
+
+        $("#survey_title").text( sname );
+        $("#survey_desc").text( sdesc );
+
+        $("#micron_at").text( questions[micron.question_index].qt );
+        $("#micron_qt").text( questions[micron.question_index].qt );
+
+        $("#micron_qb").text( questions[micron.question_index].qb );
+        $("#micron_a_qb").text( questions[micron.question_index].qb );
+
+        $("#micron_q_qid_disp").text( questions[micron.question_index].qid );
+
+        $("#micron_qid").val( questions[micron.question_index].qid );
+        $("#micron_a").val( ""/*questions[micron.question_index].a */);        
+        
+    },
+    init_button_ctrl: function() {
+        if(micron.debug_mode == true) { console.log("fxn: init_button_ctrl"); }
+        $("#answer_q").off("click").on("click", micron.answer_q );
+        $("#submit_q").off("click").on("click", micron.answer_and_get_next_q );
+     
+        $("#speech_icon_en").off("click").on("click", micron.set_recog_en);
+        $("#speech_icon_fr").off("click").on("click", micron.set_recog_fr);
+     
+        $("#speech_icon").off("click").on("click", micron.recognize_speech);
+    },
+    qs_toggle: function() {
+        if(micron.debug_mode == true) { console.log("fxn: qs_toggle"); }
+        $("#micron_thanks").hide();
+        $("#micron_loading").hide();
+        $("#micron_question").hide();
+        $("#micron_answer").hide();
+    },
+    set_init_recog: function() {
+        if(micron.debug_mode == true) { 
+            console.log("fxn: set_init_recog");
+            console.log("Recognize");
+        }
+        if( micron.recognition_lang == micron.recognition_lang_en) {
+            micron.lang_terms_file = micron.lang_terms_en;
+            micron.survey_file = micron.surveys_en;
+        } else if( micron.recognition_lang == micron.recognition_lang_fr) {
+            micron.lang_terms_file = micron.lang_terms_fr;
+            micron.survey_file = micron.surveys_fr;
+        } else {
+            micron.lang_terms_file = micron.lang_terms_en;
+            micron.survey_file = micron.surveys_en;            
+        }
+    },    
+    set_recog_en: function() {
+        if(micron.debug_mode == true) { console.log("fxn: set_recog_en"); }
+        micron.recognition_lang = micron.recognition_lang_en;
+        $("#recognition_language").val(micron.recognition_lang);
         $("#micron_listening_for").html( "micron listening in English: " +  $("#recognition_language").val() );
-        languify();
-    };
-    var set_recog_fr = function() {
-        recognition_lang = recognition_lang_fr;
-        lang_terms = lang_terms_fr;
-        $("#recognition_language").val(recognition_lang);
+        micron.init_gui();
+    },
+    set_recog_fr: function() {
+        if(micron.debug_mode == true) { console.log("fxn: set_recog_fr"); }
+        micron.recognition_lang = micron.recognition_lang_fr;
+        $("#recognition_language").val(micron.recognition_lang);
         $("#micron_listening_for").html( "micron écoute Français: " + $("#recognition_language").val() );
-        languify();
-    };
-    var recognize_speech = function() {
+        micron.init_gui();
+    },
+    recognize_speech: function() {
+        if(micron.debug_mode == true) { console.log("fxn: recognize_speech"); }
         if (window.hasOwnProperty('webkitSpeechRecognition')) {
             var recognition = new webkitSpeechRecognition();
 
@@ -44,82 +239,20 @@ $(function() {
         } else {
             $("#result_set").html("<div class='remark alert'><p><strong>micron is sorry...</strong> This page uses experimental voice recognition APIs.</p><p>It appears this browser doesnt support speech recognition. You might have more luck with <a href='https://www.google.com/chrome'>Chrome</a> or <a href='https://www.mozilla.org/firefox'>Firefox</a></p></div>");
         }
-    };
-    
-    var languify_term = function(target, langterm) {
-        $("#" + target).text(langterm);
-        console.log("" + target + " " + langterm);
-    };
+    },
+    init_gui: function() {
+        if(micron.debug_mode == true) { console.log("fxn: initgui"); }
+        micron.start_load_chain();
+    },
+    initmicron: function() {
+        if(micron.debug_mode == true) { console.log("fxn: initmicron"); }
+        micron.survey_index = 0;
+        micron.question_index = 0;
+        micron.init_gui();
+    }
+};
 
-    var languify = function() {
-        lang_terms_file = ""
-        if( recognition_lang == recognition_lang_en) {
-            lang_terms_file = lang_terms_en;
-        } else if( recognition_lang == recognition_lang_fr) {
-            lang_terms_file = lang_terms_fr;
-        } else {
-            set_recog_en();
-            lang_terms_file = lang_terms_en;
-        }
-
-        $.getJSON(lang_terms_file, function(json) {
-            lang_terms = json;
-            var lts = [
-                "lts_appname", "lts_appver", 
-                "lts_micronlisten", "lts_splashtext", "lts_footersplash", 
-                "lts_footermore", "lts_loading_msg", "lts_question_prompt", 
-                "lts_question_btn_prompt", "lts_answer_prompt", "lts_answer_btn_prompt"
-            ];
-            console.log(lang_terms); // this will show the info it in firebug console
-            for(var i = 0; i < lts.length; i++) {
-                languify_term(lts[i], lang_terms[lts[i]]);
-            }
-        });
-    };
-   
-    var qs_toggle = function() {
-        $("#micron_loading").hide();
-        $("#micron_question").hide();
-        $("#micron_answer").hide();
-    };
-    var ask_q = function() {
-        qs_toggle(); 
-        $("#micron_question").show();
-    };   
-    var answer_q = function() {
-        qs_toggle(); 
-        $("#micron_answer").show();
-    }; 
-    var answer_and_get_next_q = function() { 
-        micron_answer_text = $("#micron_a").val();
-        micron_answer_qid = $("#micron_a_qid").val();
-
-        var submission_q = [{ 
-            'qid': micron_answer_qid
-            ,'a': micron_answer_text
-        }]
-        var str_q = JSON.stringify(submission_q, null, 2);
-
-        alert("imma submit! " + str_q);
-        ask_q();
-    };
-
-    var init_gui = function() {
-    
-        $("#answer_q").on("click", answer_q );
-        $("#submit_q").on("click", answer_and_get_next_q );
-     
-        $("#speech_icon_en").on("click", set_recog_en);
-        $("#speech_icon_fr").on("click", set_recog_fr);
-     
-        $("#speech_icon").on("click", recognize_speech);
-
-        languify();
-        ask_q();
-
-    };
-
-    init_gui();
- });
+micron.initmicron(); 
+/*endjq*/ });
 
 
--- test_ui.py
+++ test_ui.py
@@ -1,4 +1,4 @@
-from flask import (Flask, render_template)
+from flask import (Flask, render_template, request)
 
 # ULTRABASIC TEMPLATE RENDERER
 # So the UI guy can build somewhat useable templates
@@ -6,6 +6,8 @@ from flask import (Flask, render_template)
 
 app_set_debug_mode = 0  # 0=none,1=entry,2=entry/exit,3=all
 app = Flask(__name__)   # just create a basic app for testing templates
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 
 # serve this up on port 5000
 @app.route('/')
@@ -15,11 +17,6 @@ def test_ui(runtype="manual-run"):
         # pass in some basic context info for the render
         context = { \
             'testname': 'ui' \
-                ,'q': [{ 'qid': 'qaz1q2w3e4r5t6y7u8i9o0pnbnvsd3' \
-                    ,'qt': 'Helpful Materials' \
-                    ,'qb': 'What TOPICS, MATERIALS, and ACTIVITIES did you find to be the most helpful?' \
-                    ,'a': '' \
-                }] \
                 ,'lts': { \
                     'pagetitle':'alpha | micron | Helping CSPS provide the best learning experiences possible.' \
                 } \
@@ -27,6 +24,10 @@ def test_ui(runtype="manual-run"):
         rendered = render_template('micron/micron.html', **context)
     return rendered
 
+@app.route('/feedback', methods = ('GET', 'POST'))
+def test_feedback():
+    return "success"
+    
 # annnd run it
 if __name__ == '__main__':
     app.run()
